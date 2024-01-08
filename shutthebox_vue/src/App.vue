5109a1d19a53c888adb3311ac7cb92f57cadbfb4
prolific-dev <de391ago@htwg-konstanz.de>
2024-01-08 23:21:32 +0100

    HW10 PWA (not ready yet)

--- shutthebox_vue/src/App.vue
+++ shutthebox_vue/src/App.vue
@@ -22,7 +22,20 @@ export default {
   components: {
     MyNavbar,
     RouterView
-}
+  },
+  created() {
+    if (this.$workbox) {
+      this.$workbox.addEventListener("waiting", () => {
+        this.showUpgradeUI = true;
+      });
+    }
+  },
+  methods: {
+    async accept() {
+      this.showUpgradeUI = false
+      await this.$workbox.messageSW({ type: "SKIP_WAITING" });
+    }
+  }
 }
 </script>
 
